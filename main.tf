434cdd2dadc4274210ca30ef8a91742eee1ccc5d
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-16 11:55:44 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,18 @@ data "vault_aws_access_credentials" "creds" {
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region     = "us-east-1"
   access_key = "${data.vault_aws_access_credentials.creds.access_key}"
   secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
 }
 
 module "mysql-server" {
-  source  = "github.com/HappyPathway/terraform-aws-mysql-server.git"
-  
+  source = "github.com/HappyPathway/terraform-aws-mysql-server.git"
+
   # git tag -a 7.0.0 -m 'no vault roles'
   # git tag -a 8.0.0 -m 'with vault roles'
   version = "8.0.0"
+
   service_name = "${var.service_name}"
-  db_name = "${var.db_name}"
+  db_name      = "${var.db_name}"
 }
