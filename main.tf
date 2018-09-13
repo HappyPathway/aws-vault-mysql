a595015d82254b0582422845577763bb71c92fd7
David Arnold <dja+djaboxx@sonic.net>
2018-09-13 12:04:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "mysql-server" {
 
   # git tag -a 7.0.0 -m 'no vault roles'
   # git tag -a 8.0.0 -m 'with vault roles'
-  version = "8.0.0"
+  version = "8.2.0"
 
   service_name = "${var.service_name}"
   db_name      = "${var.db_name}"
