df1bc7cd85b80890a9df51eaf9e81758f86f6b5a
David Arnold <dja+djaboxx@sonic.net>
2018-09-13 12:06:12 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ data "vault_aws_access_credentials" "creds" {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "${data.vault_aws_access_credentials.creds["access_key"]}"
-  secret_key = "${data.vault_aws_access_credentials.creds["secret_key"]}"
+  access_key = "${data.vault_aws_access_credentials.creds.access_key}"
+  secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
 }
 
 module "mysql-server" {
